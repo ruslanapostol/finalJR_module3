ca12a4ba08357ce5cdf393204f07d999f1add6a3
ruslanapostol <apostolruslan.java@gmail.com>
2024-07-26 10:48:32 +0300

    10th commit - created Test classes for service package classes

---
+++ src/test/java/org/javarush/apostol/jr_module3/controller/service/GameLogicServiceTest.java
@@ -0,0 +1,5 @@
+package org.javarush.apostol.jr_module3.controller.service;
+
+public class GameLogicServiceTest {
+    private GameLogicService gameLogicService;
+}
---
+++ src/test/java/org/javarush/apostol/jr_module3/controller/service/GameServiceTest.java
@@ -0,0 +1,10 @@
+package org.javarush.apostol.jr_module3.controller.service;
+
+import jakarta.servlet.http.HttpSession;
+
+
+public class GameServiceTest {
+    private GameService gameService;
+    private GameLogicService gameLogicService;
+    private HttpSession httpSession;
+}
